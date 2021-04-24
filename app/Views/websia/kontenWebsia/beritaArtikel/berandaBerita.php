3fdbad7577792ba8f15643f089f91585cbb4c094
timotiusvincent26 <timotiusvincent26@gmail.com>
2021-04-24 17:28:27 +0700

    responsive

--- app/Views/websia/kontenWebsia/beritaArtikel/berandaBerita.php
+++ app/Views/websia/kontenWebsia/beritaArtikel/berandaBerita.php
@@ -2,7 +2,7 @@
 
 <?= $this->section('content'); ?>
 
-<div class="mx-12 my-6 text-sm">
+<div class="md:mx-12 mx-6 my-6 text-sm">
     <div class="flex justify-between items-end">
         <div class="text-xs text-primary">
             <div class="flex gap-x-2">
@@ -14,7 +14,7 @@
         <div>
             <div class="flex items-center gap-x-2">
                 <a href="/User/unggahBerita">
-                    <div class="bg-secondary hover:bg-secondaryhover py-1.5 px-3 outline-none text-white rounded-full cursor-pointer">
+                    <div class="bg-secondary hover:bg-secondaryhover md:py-1.5 md:px-3 py-1 px-2 outline-none text-white rounded-full cursor-pointer">
                         Unggah Berita
                     </div>
                 </a>
@@ -25,17 +25,17 @@
         </div>
     </div>
     <hr class="border-primary mt-3">
-    <div class="mt-4 bg-primary p-6">
-        <h2 class="font-bold text-secondary text-center font-heading text-2xl">Berita Terpopuler</h2>
-        <div class="grid grid-cols-3 gap-x-6 mt-4">
-            <div class="col-span-2 bg-gray-200 h-full flex items-end p-2">
+    <div class="mt-4 bg-primary md:p-6 p-3">
+        <h2 class="font-bold text-secondary text-center font-heading lg:text-2xl md:text-xl text-lg">Berita Terpopuler</h2>
+        <div class="md:grid md:grid-cols-3 md:gap-x-6 mt-4">
+            <div class="md:col-span-2 bg-gray-200 md:h-full h-32 flex items-end p-2 sm:mb-6 mb-2 md:mb-0">
                 <h1 class="text-white font-heading font-bold text-xl">Judul berita</h1>
             </div>
-            <div class="grid grid-rows-2 gap-y-6">
-                <div class="bg-gray-200 h-48 flex items-end p-2">
+            <div class="md:grid md:grid-rows-2 md:gap-y-6">
+                <div class="bg-gray-200 lg:h-48 h-32 flex items-end p-2 sm:mb-6 mb-2 md:mb-0">
                     <h1 class="text-white font-heading font-bold text-lg">Judul berita</h1>
                 </div>
-                <div class="bg-gray-200 h-48 flex items-end p-2">
+                <div class="bg-gray-200 lg:h-48 h-32 flex items-end p-2">
                     <h1 class="text-white font-heading font-bold text-lg">Judul berita</h1>
                 </div>
             </div>
@@ -43,15 +43,17 @@
     </div>
     <hr class="border-primary mt-4 mb-3">
     <div>
-        <h2 class="font-bold text-secondary font-heading text-xl mb-4">Berita Lainnya</h2>
+        <h2 class="font-bold text-secondary font-heading md:text-xl text-lg mb-4">Berita Lainnya</h2>
         <!-- start card berita -->
-        <div class="grid grid-cols-2 gap-x-6">
+        <div class="md:grid md:grid-cols-2 md:gap-x-6">
             <div>
-                <div class="flex gap-x-2">
-                    <div class="w-1/4 h-24 bg-gray-200">
+                <div class="flex gap-x-2 items-center">
+                    <div class="lg:w-1/4 w-1/3 lg:h-24 h-20 bg-gray-200">
                     </div>
-                    <div class="w-3/4">
-                        <h3 class="font-heading font-semibold text-primary text-lg">Judul Berita</h3>
+                    <div class="lg:w-3/4 w-2/3">
+                        <a href="">
+                            <h3 class="font-heading font-semibold text-primary text-lg">Judul Berita</h3>
+                        </a>
                         <div class="flex gap-x-1 items-center">
                             <img src="/img/icon/clock.png" class="w-3 h-3" alt="">
                             <p class="text-xs text-primary">11 Januari 2021</p>
@@ -64,11 +66,13 @@
                     </div>
                 </div>
                 <hr class="my-3 border-gray-400">
-                <div class="flex gap-x-2">
-                    <div class="w-1/4 h-24 bg-gray-200">
+                <div class="flex gap-x-2 items-center">
+                    <div class="lg:w-1/4 w-1/3 lg:h-24 h-20 bg-gray-200">
                     </div>
-                    <div class="w-3/4">
-                        <h3 class="font-heading font-semibold text-primary text-lg">Judul Berita</h3>
+                    <div class="lg:w-3/4 w-2/3">
+                        <a href="">
+                            <h3 class="font-heading font-semibold text-primary text-lg">Judul Berita</h3>
+                        </a>
                         <div class="flex gap-x-1 items-center">
                             <img src="/img/icon/clock.png" class="w-3 h-3" alt="">
                             <p class="text-xs text-primary">11 Januari 2021</p>
@@ -81,11 +85,13 @@
                     </div>
                 </div>
                 <hr class="my-3 border-gray-400">
-                <div class="flex gap-x-2">
-                    <div class="w-1/4 h-24 bg-gray-200">
+                <div class="flex gap-x-2 items-center">
+                    <div class="lg:w-1/4 w-1/3 lg:h-24 h-20 bg-gray-200">
                     </div>
-                    <div class="w-3/4">
-                        <h3 class="font-heading font-semibold text-primary text-lg">Judul Berita</h3>
+                    <div class="lg:w-3/4 w-2/3">
+                        <a href="">
+                            <h3 class="font-heading font-semibold text-primary text-lg">Judul Berita</h3>
+                        </a>
                         <div class="flex gap-x-1 items-center">
                             <img src="/img/icon/clock.png" class="w-3 h-3" alt="">
                             <p class="text-xs text-primary">11 Januari 2021</p>
@@ -98,11 +104,13 @@
                     </div>
                 </div>
                 <hr class="my-3 border-gray-400">
-                <div class="flex gap-x-2">
-                    <div class="w-1/4 h-24 bg-gray-200">
+                <div class="flex gap-x-2 items-center">
+                    <div class="lg:w-1/4 w-1/3 lg:h-24 h-20 bg-gray-200">
                     </div>
-                    <div class="w-3/4">
-                        <h3 class="font-heading font-semibold text-primary text-lg">Judul Berita</h3>
+                    <div class="lg:w-3/4 w-2/3">
+                        <a href="">
+                            <h3 class="font-heading font-semibold text-primary text-lg">Judul Berita</h3>
+                        </a>
                         <div class="flex gap-x-1 items-center">
                             <img src="/img/icon/clock.png" class="w-3 h-3" alt="">
                             <p class="text-xs text-primary">11 Januari 2021</p>
@@ -114,13 +122,16 @@
                         </p>
                     </div>
                 </div>
+                <hr class="my-3 border-gray-400">
             </div>
-            <div>
-                <div class="flex gap-x-2">
-                    <div class="w-1/4 h-24 bg-gray-200">
+            <div class="md:block hidden">
+                <div class="flex gap-x-2 items-center">
+                    <div class="lg:w-1/4 w-1/3 lg:h-24 h-20 bg-gray-200">
                     </div>
-                    <div class="w-3/4">
-                        <h3 class="font-heading font-semibold text-primary text-lg">Judul Berita</h3>
+                    <div class="lg:w-3/4 w-2/3">
+                        <a href="">
+                            <h3 class="font-heading font-semibold text-primary text-lg">Judul Berita</h3>
+                        </a>
                         <div class="flex gap-x-1 items-center">
                             <img src="/img/icon/clock.png" class="w-3 h-3" alt="">
                             <p class="text-xs text-primary">11 Januari 2021</p>
@@ -133,11 +144,13 @@
                     </div>
                 </div>
                 <hr class="my-3 border-gray-400">
-                <div class="flex gap-x-2">
-                    <div class="w-1/4 h-24 bg-gray-200">
+                <div class="flex gap-x-2 items-center">
+                    <div class="lg:w-1/4 w-1/3 lg:h-24 h-20 bg-gray-200">
                     </div>
-                    <div class="w-3/4">
-                        <h3 class="font-heading font-semibold text-primary text-lg">Judul Berita</h3>
+                    <div class="lg:w-3/4 w-2/3">
+                        <a href="">
+                            <h3 class="font-heading font-semibold text-primary text-lg">Judul Berita</h3>
+                        </a>
                         <div class="flex gap-x-1 items-center">
                             <img src="/img/icon/clock.png" class="w-3 h-3" alt="">
                             <p class="text-xs text-primary">11 Januari 2021</p>
@@ -150,11 +163,13 @@
                     </div>
                 </div>
                 <hr class="my-3 border-gray-400">
-                <div class="flex gap-x-2">
-                    <div class="w-1/4 h-24 bg-gray-200">
+                <div class="flex gap-x-2 items-center">
+                    <div class="lg:w-1/4 w-1/3 lg:h-24 h-20 bg-gray-200">
                     </div>
-                    <div class="w-3/4">
-                        <h3 class="font-heading font-semibold text-primary text-lg">Judul Berita</h3>
+                    <div class="lg:w-3/4 w-2/3">
+                        <a href="">
+                            <h3 class="font-heading font-semibold text-primary text-lg">Judul Berita</h3>
+                        </a>
                         <div class="flex gap-x-1 items-center">
                             <img src="/img/icon/clock.png" class="w-3 h-3" alt="">
                             <p class="text-xs text-primary">11 Januari 2021</p>
@@ -167,11 +182,13 @@
                     </div>
                 </div>
                 <hr class="my-3 border-gray-400">
-                <div class="flex gap-x-2">
-                    <div class="w-1/4 h-24 bg-gray-200">
+                <div class="flex gap-x-2 items-center">
+                    <div class="lg:w-1/4 w-1/3 lg:h-24 h-20 bg-gray-200">
                     </div>
-                    <div class="w-3/4">
-                        <h3 class="font-heading font-semibold text-primary text-lg">Judul Berita</h3>
+                    <div class="lg:w-3/4 w-2/3">
+                        <a href="">
+                            <h3 class="font-heading font-semibold text-primary text-lg">Judul Berita</h3>
+                        </a>
                         <div class="flex gap-x-1 items-center">
                             <img src="/img/icon/clock.png" class="w-3 h-3" alt="">
                             <p class="text-xs text-primary">11 Januari 2021</p>
@@ -183,6 +200,7 @@
                         </p>
                     </div>
                 </div>
+                <hr class="my-3 border-gray-400">
             </div>
         </div>
         <!-- end card berita -->
