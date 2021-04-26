fd1670d200df0a61c6b79d39ddd5dd68b567737f
timotiusvincent26 <timotiusvincent26@gmail.com>
2021-04-26 17:17:02 +0700

    benerin tampilan editprofilWS

--- app/Views/webservice/kontenWebservice/profilDeveloper/editAkunWS.php
+++ app/Views/webservice/kontenWebservice/profilDeveloper/editAkunWS.php
@@ -2,14 +2,23 @@
 
 <?= $this->section('contentEdit'); ?>
 <div class="flex justify-center mb-8">
-    <div class="shadow-2xl rounded-xl md:p-16 p-4 lg:w-full w-5/6">
+    <div class="shadow-2xl rounded-xl p-6 lg:w-full w-5/6">
         <form action="" method="POST" class="font-paragraph text-primary" id="formEditAkunDev">
             <div class="lg:w-1/2 w-full">
+                <!--nanti data di sini value nya ngambil dari db nya sin, sama pesan errornya ditampilin pas validasi
+                pas klik simpan nanti-->
+                <!--kayaknya belum ada fungsi buat simpan sama validasinya juga di controller -->
                 <label for="email" class="font-medium">Email:</label>
-                <input type="text" name="email" id="email" class="inputForm mb-2" placeholder="mail@example.com">
+                <div class="text-black font-heading font-normal mb-2">iniemail@stis.ac.id</div>
                 <label for="passbaru" class="font-medium">Kata Sandi Baru:</label>
+                <p class="text-xs text-red-500 text-justify" id="errorPassBaru">
+                    Kata sandi harus terdiri dari huruf dan angka.
+                </p>
                 <input type="password" name="passbaru" id="passbaru" class="inputForm mb-2" placeholder="🞄🞄🞄🞄🞄🞄🞄🞄">
                 <label for="ulangpassbaru" class="font-medium">Ketik Ulang Kata Sandi Baru:</label>
+                <p class="text-xs text-red-500 text-justify" id="errorValidPasss">
+                    Kata sandi tidak sesuai.
+                </p>
                 <input type="password" name="ulangpassbaru" id="ulangpassbaru" class="inputForm mb-2" placeholder="🞄🞄🞄🞄🞄🞄🞄🞄">
                 <div class="text-secondary text-justify text-xs mt-6 mb-2">
                     Silakan Masukkan Kata Sandi Lama Anda untuk Verifikasi!
@@ -18,7 +27,7 @@
                 <input type="password" name="passlama" id="passlama" class="inputForm mb-2" placeholder="🞄🞄🞄🞄🞄🞄🞄🞄">
             </div>
             <div class="flex justify-end">
-                <input type="submit" value="SIMPAN" class="w-24 text-center py-1 bg-secondary hover:bg-secondaryhover text-white rounded-full cursor-pointer focus:outline-none mt-8 md:text-base text-sm" id="simpanAkun">
+                <input type="submit" value="SIMPAN" class="w-24 text-center py-1 bg-secondary hover:bg-secondaryhover text-white rounded-full cursor-pointer focus:outline-none mt-8 text-sm" id="simpanAkun">
             </div>
         </form>
     </div>
--- app/Views/webservice/kontenWebservice/profilDeveloper/editBiodataWS.php
+++ app/Views/webservice/kontenWebservice/profilDeveloper/editBiodataWS.php
@@ -2,7 +2,7 @@
 
 <?= $this->section('contentEdit'); ?>
 <div class="flex justify-center mb-8">
-    <div class="shadow-2xl rounded-xl md:p-16 p-4 lg:w-full w-5/6">
+    <div class="shadow-2xl rounded-xl p-6 lg:w-full w-5/6">
         <form action="" method="POST" class="font-paragraph text-primary" id="formEditBiodataDev">
             <label for="nama" class="font-medium">Nama:</label>
             <input type="text" name="nama" id="nama" class="inputForm mb-2" placeholder="Nama Lengkap">
@@ -21,7 +21,7 @@
             <label for="instansi" class="font-medium">Instansi:</label>
             <input type="text" name="instansi" id="instansi" class="inputForm mb-2" placeholder="Politeknik Statistika STIS">
             <div class="flex justify-end">
-                <input type="submit" value="SIMPAN" class="w-24 text-center py-1 bg-secondary hover:bg-secondaryhover text-white rounded-full cursor-pointer focus:outline-none mt-8 md:text-base text-sm" id="simpanBiodata">
+                <input type="submit" value="SIMPAN" class="w-24 text-center py-1 bg-secondary hover:bg-secondaryhover text-white rounded-full cursor-pointer focus:outline-none mt-8 text-sm" id="simpanBiodata">
             </div>
         </form>
     </div>
