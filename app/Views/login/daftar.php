5c990666d39359a6c4284bd6973b994b0c6ee7c6
zahlulapaaja <zahlul.2001@gmail.com>
2021-06-03 20:25:32 +0700

    masukin infografis pendaftaran

--- app/Views/login/daftar.php
+++ app/Views/login/daftar.php
@@ -6,7 +6,7 @@
     <h3 class="font-bold text-primary lg:text-3xl md:text-2xl text-xl sm:block flex justify-center text-center sm:text-left">Panduan Pendaftaran Sistem Informasi Alumni</h3>
     <a href="/login/" class="text-secondary sm:text-base font-medium sm:block hidden hover:text-yellow-700 w-max">Kembali ke laman login</a>
     <div class="flex justify-center mt-4 sm:pb-4 pb-2">
-        <img src="/img/components/daftar.jpg" alt="Daftar">
+        <img src="/img/components/daftar.png" alt="Daftar">
     </div>
     <div class="sm:hidden pb-3 text-right">
         <a href="/login/" class="text-secondary text-sm font-medium hover:text-yellow-700">Kembali ke laman login</a>
