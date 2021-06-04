c0925916a4ae6dfb2c9efaa709283e5d226d9c51
ihkaru <ihza2karunia@gmail.com>
2021-06-04 10:32:31 +0700

    menambah controller tes dan fungsi csv ke array

--- app/Helpers/standarisasi_helper.php
+++ app/Helpers/standarisasi_helper.php
@@ -277,6 +277,7 @@ function standardBPS($key)
 }
 
 # Helper to standardize Tempat Lahir
+
 function standardTempatLahir($key)
 {
     $arr_standar = [
@@ -325,4 +326,25 @@ function standardTempatLahir($key)
     }
 
     return $value;
+}
+
+function csv_to_array($filename='', $delimiter=',')
+{
+if(!file_exists($filename) || !is_readable($filename))
+return FALSE;
+
+$header = NULL;
+$data = array();
+if (($handle = fopen($filename, 'r')) !== FALSE)
+{
+while (($row = fgetcsv($handle, 1000, $delimiter)) !== FALSE)
+{
+if(!$header)
+$header = $row;
+else
+$data[] = array_combine($header, $row);
+}
+fclose($handle);
+}
+return $data;
 }
