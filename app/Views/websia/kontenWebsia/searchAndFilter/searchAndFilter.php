94e345a435f38b91a64b925a44461ffaee013eac
Mochamaddiaz <221810421@stis.ac.id>
2021-06-06 14:47:34 +0700

    revisi sidebaer

--- app/Views/websia/kontenWebsia/searchAndFilter/searchAndFilter.php
+++ app/Views/websia/kontenWebsia/searchAndFilter/searchAndFilter.php
@@ -13,7 +13,7 @@
         <div class="flex">
 
             <!-- awal -> ini hanya untuk margin sidebar jadi jangan ubah kecuali jika ubah ukuran sidebarnya  -->
-            <div class=" invisible">
+            <div class="md:hidden invisible">
                 <div class="flex md:px-4 px-2 py-1 justify-between bg-primaryHover marginSidebar">
                     <svg class="w-4 fill-current text-secondary cursor-pointer" id="hamburgerSidebar" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
