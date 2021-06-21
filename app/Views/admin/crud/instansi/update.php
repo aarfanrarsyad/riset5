2ef05125a53a2be9da272b480ba044f132fe4d5e
kiranhasibuan <59794872+kiranhasibuan@users.noreply.github.com>
2021-06-21 23:28:44 +0700

    fix bugs

--- app/Views/admin/crud/instansi/update.php
+++ app/Views/admin/crud/instansi/update.php
@@ -30,67 +30,102 @@
                                     <h5><i class="fas fa-building text-primaryHover"></i>&ensp;Form Update Instasi</h5>
                                 </div>
                             </div>
-                            <br>
-                            <form action="/admin/updateInstansi" method="post" class="row g-3">
-                                <?= csrf_field(); ?>
-                                <input type="hidden" id="id_tempat_kerja" name="id_tempat_kerja" value="<?= $instansi->id_tempat_kerja; ?>">
-                                <div class="col-12">
-                                    <label for="nama_instansi" class="col-sm-2 text-sm col-form-label">Nama Instansi</label>
-                                    <input type="text" class="form-control inputForm <?= ($validation->hasError('nama_instansi')) ? 'is-invalid' : ''; ?>" id="nama_instansi" name="nama_instansi" value="<?= $instansi->nama_instansi; ?>">
-                                    <div class="invalid-feedback">
-                                        <?= $validation->getError('nama_instansi'); ?>
+                            <br>                        
+                            <form action="/admin/updateInstansi/<?= $instansi->id_tempat_kerja; ?>" method="POST">
+                            <?= csrf_field(); ?>                                
+                                <label for="nama" class="font-medium">Nama Instansi:</label>
+                                <?php if (session()->getFlashdata('error-nama_instansi') != "") { ?>
+                                    <p class="text-xs text-red-500 text-justify" id="errorNamaInstansi">
+                                        <?= session('error-nama_instansi') ?>
+                                    </p>
+                                <?php } ?>
+                                <input type="text" name="nama_instansi" id="nama" class="inputForm" placeholder="Masukkan nama Instansi" value="<?= $instansi->nama_instansi; ?>">
+                                <div class="flex justify-between items-center">
+                                    <label for="negara" class="font-medium" id="labelNegara">Negara:</label>    
+                                </div>
+                                    <select name="negara" id="negara" class="inputForm" onchange="displayDiv2('negaraLainIndonesia','negaraIndonesia',this)">
+                                        <?php if ($instansi->negara == NULL) : ?>
+                                            <option disabled selected>Pilih Negara</option>
+                                            <option value="Indonesia">Indonesia</option>
+                                            <option value="lainnya">Lainnya...</option>
+                                        <?php elseif ($instansi->negara == "Indonesia") : ?>
+                                            <option selected value="Indonesia">Indonesia</option>
+                                            <option value="lainnya">Lainnya...</option>
+                                        <?php else : ?>
+                                            <option value="<?= $instansi->negara ?>" selected>
+                                                <?= $instansi->negara ?>
+                                            </option>
+                                            <option value="Indonesia">Indonesia</option>
+                                            <option value="lainnya">Lainnya...</option>
+                                        <?php endif; ?>
+                                    </select>
+                                <div class="hidden" id="negaraLainIndonesia">
+                                    <input type="text" name="negaraLainnya" id="negaraLainnya" class="inputForm" placeholder="Masukkan nama negara">
+                                </div>
+                                <div class="hidden" id="negaraIndonesia">
+                                    <div class="md:grid md:grid-cols-2 md:gap-x-4">
+                                        <div>
+                                            <label for="provinsi" class="font-medium" id="labelProvinsi">Provinsi:</label>
+                                            <select name="provinsi" id="nama-provinsi" class="inputForm">
+                                                <?php if ($instansi->provinsi != NULL) : ?>
+                                                    <option value="<?= $instansi->provinsi ?>" selected>
+                                                        <?= $instansi->provinsi ?>
+                                                    </option>
+                                                    <?php foreach ($daftarProv as $prov) : ?>
+                                                        <option id="<?= $prov->id_provinsi ?>" value="<?= $prov->nama_provinsi ?>"><?= $prov->nama_provinsi ?></option>
+                                                    <?php endforeach ?>
+                                                <?php else : ?>
+                                                    <option selected disabled>Pilih Provinsi</option>
+                                                    <?php foreach ($daftarProv as $prov) : ?>
+                                                        <option id="<?= $prov->id_provinsi ?>" value="<?= $prov->nama_provinsi ?>"><?= $prov->nama_provinsi ?></option>
+                                                    <?php endforeach ?>
+                                                <?php endif; ?>
+                                            </select>
+                                            <input hidden type="text" name="prov" id="prov-hidden">
+                                        </div>
+                                        <div>
+                                            <label for='kabkota' class='font-medium' id='labelKabkot'>Kabupaten/Kota:</label>
+                                            <select name='kabkota' id='kabkota' class='inputForm'>
+                                                <?php if ($instansi->kota != NULL) : ?>
+                                                    <option value="<?= $instansi->kota ?>" selected>
+                                                        <?= $instansi->kota ?>
+                                                    </option>
+                                                <?php else : ?>
+                                                    <option selected disabled>Pilih Kabupaten/Kota</option>
+                                                <?php endif; ?>
+                                            </select>
+                                            <input hidden type="text" name="kab" id="kab-hidden">
+                                        </div>
                                     </div>
                                 </div>
-                                <div class="col-12">
-                                    <label for="alamat_instansi" class="col-sm-3 text-sm col-form-label">Alamat Instansi</label>
-                                    <textarea class="form-control inputForm <?= ($validation->hasError('alamat_instansi')) ? 'is-invalid' : ''; ?>" id="alamat_instansi" name="alamat_instansi" rows="2"><?= $instansi->alamat_instansi; ?></textarea>
-                                    <div class="invalid-feedback">
-                                        <?= $validation->getError('alamat_instansi'); ?>
-                                    </div>
+                                <label for="alamat" class="font-medium">Alamat Instansi:</label>
+                                <textarea type="text" name="alamat_instansi" id="alamat" class="inputForm resize-none" placeholder="Masukkan alamat instansi"><?= $instansi->alamat_instansi; ?></textarea>
+                                <div class="md:w-1/2 w-full">
+                                    <label for="telepon" class="font-medium">No Telepon Instansi:</label>
+                                    <?php if (session()->getFlashdata('error-telp_instansi') != "") { ?>
+                                        <p class="text-xs text-red-500 text-justify" id="errorEmailInstansi">
+                                            <?= session('error-telp_instansi') ?>
+                                        </p>
+                                    <?php } ?>
+                                    <input type="text" name="telp_instansi" id="telepon" class="inputForm" placeholder="Masukkan telepon instansi" value="<?= $instansi->telp_instansi; ?>">
+                                    <label for="faks" class="font-medium">Faks Instansi:</label>
+                                    <?php if (session()->getFlashdata('error-faks_instansi') != "") { ?>
+                                        <p class="text-xs text-red-500 text-justify" id="errorEmailInstansi">
+                                            <?= session('error-faks_instansi') ?>
+                                        </p>
+                                    <?php } ?>
+                                    <input type="text" name="faks_instansi" id="faks" class="inputForm" placeholder="Masukkan faks instansi" value="<?= $instansi->faks_instansi; ?>">
+                                    <label for="email" class="font-medium">Email:</label>
+                                    <?php if (session()->getFlashdata('error-email_instansi') != "") { ?>
+                                        <p class="text-xs text-red-500 text-justify" id="errorEmailInstansi">
+                                            <?= session('error-email_instansi') ?>
+                                        </p>
+                                    <?php } ?>
+                                    <input type="email" name="email_instansi" id="email" class="inputForm" placeholder="Masukkan email instansi" value="<?= $instansi->email_instansi; ?>">
                                 </div>
-                                <div class="col-12">
-                                    <label for="negara" class="col-sm-2 text-sm col-form-label">Negara</label>
-                                    <input type="text" class="form-control inputForm <?= ($validation->hasError('negara')) ? 'is-invalid' : ''; ?>" id="negara" name="negara" value="<?= $instansi->negara; ?>">
-                                    <div class="invalid-feedback">
-                                        <?= $validation->getError('negara'); ?>
-                                    </div>
-                                </div>
-                                <div class="col-12">
-                                    <label for="provinsi" class="col-sm-2 text-sm col-form-label">Provinsi</label>
-                                    <input type="text" class="form-control inputForm <?= ($validation->hasError('provinsi')) ? 'is-invalid' : ''; ?>" id="provinsi" name="provinsi" value="<?= $instansi->provinsi; ?>">
-                                    <div class="invalid-feedback">
-                                        <?= $validation->getError('provinsi'); ?>
-                                    </div>
-                                </div>
-                                <div class="col-12">
-                                    <label for="kota" class="col-sm-2 text-sm col-form-label">Kota/Kabupaten</label>
-                                    <input type="text" class="form-control inputForm <?= ($validation->hasError('kota')) ? 'is-invalid' : ''; ?>" id="kota" name="kota" value="<?= $instansi->kota; ?>">
-                                    <div class="invalid-feedback">
-                                        <?= $validation->getError('kota'); ?>
-                                    </div>
-                                </div>
-                                <div class="col-md-6">
-                                    <label for="telp_instansi" class="col-sm-6 text-sm col-form-label">No Telepon Instansi</label>
-                                    <input type="text" class="form-control inputForm <?= ($validation->hasError('telp_instansi')) ? 'is-invalid' : ''; ?>" id="telp_instansi" name="telp_instansi" value="<?= $instansi->telp_instansi; ?>">
-                                    <div class="invalid-feedback">
-                                        <?= $validation->getError('telp_instansi'); ?>
-                                    </div>
-                                </div>
-                                <div class="col-md-6">
-                                    <label for="faks_instansi" class="col-sm-4 text-sm col-form-label">Faks Instansi</label>
-                                    <input type="text" class="form-control inputForm <?= ($validation->hasError('faks_instansi')) ? 'is-invalid' : ''; ?>" id="faks_instansi" name="faks_instansi" value="<?= $instansi->faks_instansi; ?>">
-                                    <div class="invalid-feedback">
-                                        <?= $validation->getError('faks_instansi'); ?>
-                                    </div>
-                                </div>
-                                <div class="col-12">
-                                    <label for="email_instansi" class="col-sm-2 text-sm col-form-label">Email</label>
-                                    <input type="email" class="form-control inputForm <?= ($validation->hasError('email_instansi')) ? 'is-invalid' : ''; ?>" id="email_instansi" name="email_instansi" value="<?= $instansi->email_instansi; ?>">
-                                    <div class="invalid-feedback">
-                                        <?= $validation->getError('email_instansi'); ?>
-                                    </div>
-                                </div>
-                                <button type="submit" class="btn btn-primary row my-3 ml-2">Update Instansi</button>
+                                <div class="flex justify-end md:mb-6 mt-12">
+                                    <input type="submit" value="SIMPAN" class="w-24 text-center py-1 bg-primarySidebar hover:bg-secondaryhover text-white rounded-full cursor-pointer focus:outline-none" id="tambahTempatKerja">
+                                </div>                                                    
                             </form>
                         </div>
                     </div>
