489715503274ab4c0dc50913591f5f8357e77c26
Mutia Soraya Adila <mutsorayaa@gmail.com>
2021-06-25 10:51:02 +0700

    Ngubah yang ada redirect webnya

--- app/Views/websia/layoutWebsia/templateBerandaLogin.php
+++ app/Views/websia/layoutWebsia/templateBerandaLogin.php
@@ -17,18 +17,23 @@
     <script type="text/javascript" src="/js/jquery.js"></script>
     <script type="text/javascript" src="/js/leaflet.js"></script>
 
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <!-- <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script> -->
+    <script type="text/javascript" src="/js/jquery.min.js"></script>
+    <script type="text/javascript" src="/js/bootstrap.min.js"></script>
 
 
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <!-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"> -->
+    <link rel="stylesheet" href="/css/font-awesome.min.css">
+
     <link rel="stylesheet" href="/css/add_style.css">
     <link rel="shortcut icon" type="image/png" href="/img/components/logo/logo_sia.png" />
 
 
     <!-- link utk manggil font nya  -->
-    <link rel="preconnect" href="https://fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css2?family=Cabin:ital,wght@0,400;0,500;0,600;0,700;1,400;1,500;1,600;1,700&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+    <!-- <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Cabin:ital,wght@0,400;0,500;0,600;0,700;1,400;1,500;1,600;1,700&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet"> -->
+    <link rel="stylesheet" href="/css/font.css">
 
     <!-- summernote For Form Upload -->
     <link rel="stylesheet" href="<?= base_url() ?>/vendor/AdminLTE/plugins/summernote/summernote-bs4.min.css">
@@ -40,7 +45,8 @@
     <?php endif; ?>
 
     <?php if ($judulHalaman == "Berita") : ?>
-        <script src="https://cdn.jsdelivr.net/npm/sweetalert2@10"></script>
+        <!-- <script src="https://cdn.jsdelivr.net/npm/sweetalert2@10"></script> -->
+        <script src="/js/sweetalert2@10"></script>
         <link rel="stylesheet" href="<?= base_url() ?>/vendor/AdminLTE/plugins/fontawesome-free/css/all.min.css">
         <script src="/js/selectize.js"></script>
     <?php endif; ?>
