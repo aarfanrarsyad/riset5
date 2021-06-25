b3ba9de19b3cbbc87249e23ec8dcc4f527488ead
Xeryus <akhmadfadilm@gmail.com>
2021-06-25 08:57:18 +0700

    update sql sama tombol back di error

--- app/Views/errors/html/error_403.php
+++ app/Views/errors/html/error_403.php
@@ -10,9 +10,9 @@
         </div>
         <div class="text-primary text-center font-heading font-bold md:text-xl text-xs -mt-8 mx-auto">Maaf, Anda memerlukan akses untuk mengunjungi halaman ini</div>
         <hr class="border-b-2 border-t-0 w-32 border-gray-400 mx-auto">
-        <a href="">
-            <button class="mt-4 py-1 px-6 text-right rounded-3xl bg-secondary border-2 border-secondary text-white hover:bg-white hover:border-2 hover:border-secondary hover:text-secondary transition-colors duration-300 font-paragraph md:text-xl text-xs focus:outline-none">Kembali</button>
-        </a>
+        <script>
+            document.write('<a href="' + document.referrer + '"><button class="mt-4 py-1 px-6 text-right rounded-3xl bg-secondary border-2 border-secondary text-white hover:bg-white hover:border-2 hover:border-secondary hover:text-secondary transition-colors duration-300 font-paragraph md:text-xl text-xs focus:outline-none">Kembali</button></a>');
+        </script>
     </div>
 
 </div>
